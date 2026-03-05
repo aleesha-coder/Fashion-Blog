132e6fe2f8943312a72234359419478c08e45636
Aleesha Praemchand <aleeshapraemchand8@gmail.com>
2026-03-05 19:16:28 +0200

    Added to Home Page

--- obj/Debug/net9.0/Fashion Blog.AssemblyInfo.cs
+++ obj/Debug/net9.0/Fashion Blog.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Fashion Blog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b69b0fca1169ca852f00fa10091cb91ed83637d5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4edc27e583d661d4f2ae1269666d794610000098")]
 [assembly: System.Reflection.AssemblyProductAttribute("Fashion Blog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Fashion Blog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
