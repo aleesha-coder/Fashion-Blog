40d74a2300eeebdf26b6e0ef79c25fa8bdd995db
Aleesha Praemchand <aleeshapraemchand8@gmail.com>
2026-03-06 22:15:57 +0200

    Made changes to allow Images to appear on Home Page

--- obj/Debug/net9.0/Fashion Blog.AssemblyInfo.cs
+++ obj/Debug/net9.0/Fashion Blog.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Fashion Blog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a39657c92fad1340a7b8a7c4cae54916cb04773")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+302ccd8bb18128c1e75e01a948167fe2d932038f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Fashion Blog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Fashion Blog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
